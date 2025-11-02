6aa99810c1f007daf762128a77f920341426f974
Uliana Sozinova <uliana.so-dev@yandex.ru>
2025-11-03 00:58:01 +0400

    The architecture and data are formed

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
+from .constants import ROOMS
+
+
+game_state = {
+    'player_inventory': [],
+    'current_room': 'entrance',
+    'game_over': False,
+    'steps_taken': 0
+}
+  
 def main():
     print("Первая попытка запустить проект!")
